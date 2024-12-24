860bf72b30f9325b9ef31422394165145b1b3bd6
anindya033 <anindya033paul@gmail.com>
2024-12-24 17:25:48 +0530

    Microservice : Implement config service

---
+++ src/test/java/com/crafter/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crafter.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
